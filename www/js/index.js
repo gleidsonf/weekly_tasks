305a1f0859ff0d669415fb95e22bcef2ee6f7d5f
Gleidson Silva <gleidson.fgs@gmail.com>
2018-01-04 00:12:23 -0300

    Day 03/100

--- www/js/index.js
+++ www/js/index.js
@@ -4,7 +4,10 @@ let instance = M.Tabs.init(document.querySelector('.tabs'), {
 
 let elem = document.querySelector('.modal');
 let instanceModal = M.Modal.init(elem, {
-    opacity: 0.5
+    opacity: 0.5,
+    onCloseEnd: function() {
+      document.querySelector('form').reset();
+    }
 })
 
 let week = [
@@ -24,24 +27,30 @@ action.addEventListener('click', function() {
     let taskName = document.querySelector('#taskname');
     let day_week = document.querySelector('[type=radio]:checked');
     configureCheckbox(taskName, day_week);
-    console.log(taskName.value);
-    document.querySelector('#taskname').textContent = '';
-    document.getElementsByName('week')[0].checked=true
+    document.querySelector('form').reset();
 });
 
 
 configureCheckbox = function(taskName, day_week) {
   let day_name = weekMap.get(parseInt(day_week.value));
-  let divDay = document.getElementById(day_name);
+  // let divDay = document.getElementById(day_name);
+  let list = document.querySelector(`#${day_name} ul`);
+  let list_item = document.createElement('li');
   let label = document.createElement('label');
   let input = document.createElement('input');
   input.setAttribute('type', 'checkbox');
   input.setAttribute('class', 'filled-in');
   input.setAttribute('name', day_name + '_option');
+  let badge = document.createElement('span');
+  badge.setAttribute('class', 'new badge');
+  badge.textContent = new Date().toLocaleDateString();
   let span = document.createElement('span');
   span.textContent = taskName.value;
   label.appendChild(input);
   label.appendChild(span);
-  divDay.appendChild(label);
+  label.appendChild(badge);
+  list_item.appendChild(label);
+  list.appendChild(list_item);
+  // divDay.appendChild(label);
 
 };
