19be6456dd86b67b642f07530752fac979257376
Gleidson Silva <gleidson.fgs@gmail.com>
2018-01-07 09:30:04 -0300

    Day 04/100

--- www/js/index.js
+++ www/js/index.js
@@ -1,56 +1,114 @@
-let instance = M.Tabs.init(document.querySelector('.tabs'), {
-    duration: 500
-})
+String.prototype.capitalize = function() {
+  return this.charAt(0).toUpperCase() + this.slice(1);
+}
 
-let elem = document.querySelector('.modal');
-let instanceModal = M.Modal.init(elem, {
-    opacity: 0.5,
-    onCloseEnd: function() {
-      document.querySelector('form').reset();
-    }
-})
+
+// let instance = M.Tabs.init(document.querySelector('.tabs'), {
+//     duration: 500
+// })
+//
+// let elem = document.querySelector('.modal');
+// let instanceModal = M.Modal.init(elem, {
+//     opacity: 0.5,
+//     onCloseEnd: function() {
+//       document.querySelector('form').reset();
+//     }
+// })
+
+var elem = document.querySelector('.sidenav');
+var instance = M.Sidenav.init(elem, {});
+
+
 
 let week = [
-    [1, 'sunday'],
-    [2, 'monday'],
-    [3, 'tuesday'],
-    [4, 'wednesday'],
-    [5, 'thursday'],
-    [6, 'friday'],
-    [7, 'saturday']
+  [1, 'sunday'],
+  [2, 'monday'],
+  [3, 'tuesday'],
+  [4, 'wednesday'],
+  [5, 'thursday'],
+  [6, 'friday'],
+  [7, 'saturday']
 ];
 let weekMap = new Map(week);
 
 let action = document.querySelector('.modal-action');
 
 action.addEventListener('click', function() {
-    let taskName = document.querySelector('#taskname');
-    let day_week = document.querySelector('[type=radio]:checked');
-    configureCheckbox(taskName, day_week);
-    document.querySelector('form').reset();
+  let taskName = document.querySelector('#taskname');
+  let day_week = document.querySelector('[type=radio]:checked');
+  configureCheckbox(taskName, day_week);
+  document.querySelector('form').reset();
+  let week = document.querySelector('#week');
+  week.setAttribute('class', 'container');
+  document.querySelector('#taskform').setAttribute('class', 'hide');
+  instance.close();
+
 });
 
+let newtask = document.querySelector('#newtask');
+
+newtask.addEventListener('click', function() {
+  let week = document.querySelector('#week');
+  week.setAttribute('class', 'hide');
+  document.querySelector('#taskform').setAttribute('class', 'container');
+  instance.close();
+});
+
+let back_to_week = document.querySelector('#back_week');
+back_to_week.addEventListener('click', function() {
+  document.querySelector('#taskform').setAttribute('class', 'hide');
+  document.querySelector('#week').setAttribute('class', 'container');
+});
+
+function dateToStatus(date) {
+  current_date = new Date().toLocaleDateString();
+  if (date == current_date) {
+    return 'Today';
+  } else if (date < current_date) {
+    return 'Overdue';
+  }
+  return 'Afterwards';
+}
+
+function short(day_name) {
+  return day_name.capitalize().substring(0, 3);
+}
 
 configureCheckbox = function(taskName, day_week) {
-  let day_name = weekMap.get(parseInt(day_week.value));
-  // let divDay = document.getElementById(day_name);
-  let list = document.querySelector(`#${day_name} ul`);
-  let list_item = document.createElement('li');
+
+  let tr = document.createElement('tr');
+  let td1 = document.createElement('td');
+  let td2 = document.createElement('td');
+  let td3 = document.createElement('td');
   let label = document.createElement('label');
   let input = document.createElement('input');
+  let span1 = document.createElement('span');
+  let span2 = document.createElement('span');
+  let day_name = weekMap.get(parseInt(day_week.value));
+
   input.setAttribute('type', 'checkbox');
-  input.setAttribute('class', 'filled-in');
-  input.setAttribute('name', day_name + '_option');
-  let badge = document.createElement('span');
-  badge.setAttribute('class', 'new badge');
-  badge.textContent = new Date().toLocaleDateString();
-  let span = document.createElement('span');
-  span.textContent = taskName.value;
+  span1.textContent = short(day_name);
   label.appendChild(input);
-  label.appendChild(span);
-  label.appendChild(badge);
-  list_item.appendChild(label);
-  list.appendChild(list_item);
-  // divDay.appendChild(label);
+  label.appendChild(span1);
+  td1.appendChild(label);
+
+  td2.textContent = taskName.value;
+
+  span2.textContent = new Date().toLocaleDateString();
+  span2.setAttribute('class', 'hide');
+  td3.textContent = dateToStatus(span2.textContent);
+  td3.appendChild(span2);
+  td3.setAttribute('class', 'status');
+
+  tr.setAttribute('class', 'today');
+  tr.appendChild(td1);
+  tr.appendChild(td2);
+  tr.appendChild(td3);
+
+  let list = document.querySelector(`#${day_name}`);
+
+  list.appendChild(tr);
+
+
 
 };
---
+++ www/js/initializers.js
@@ -0,0 +1,14 @@
+// let instance = M.Tabs.init(document.querySelector('.tabs'), {
+//     duration: 500
+// })
+
+// let elem = document.querySelector('.modal');
+// let instanceModal = M.Modal.init(elem, {
+//     opacity: 0.5,
+//     onCloseEnd: function() {
+//       document.querySelector('form').reset();
+//     }
+// });
+
+let elem = document.querySelector('.sidenav');
+let instance = M.Sidenav.init();
