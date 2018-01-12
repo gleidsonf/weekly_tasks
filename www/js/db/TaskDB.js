c39b624bb75e0acd0493d66e1d62d7a45ded62c1
Gleidson Silva <gleidson.fgs@gmail.com>
2018-01-12 00:43:50 -0300

    Day 09/100

---
+++ www/js/db/TaskDB.js
@@ -0,0 +1,41 @@
+class TaskDB {
+
+  constructor() {
+    this._db = window.sqlitePlugin.openDatabase({name: "wk.db", location: 2, createFromLocation: 1
+    });
+
+    this._db.transaction(function(tx) {
+      //create table
+      tx.executeSql("CREATE TABLE IF NOT EXISTS task (day text, title text, date_task text)", [], function(tx, res){
+      }
+    }
+  }
+
+  get db() {
+    return this._db;
+  }
+
+  insert(model) {
+    this._db.transaction(function(tx) {
+      tx.executeSql("INSERT INTO task (day, title, date_task) VALUES (?,?,?)", [${model.dayID}, ${model.title}, ${model.date_task}], function(tx, res) {
+        console.log('Operação encerrada.');
+      });
+    });
+  }
+
+  get tasks() {
+    let arr = [];
+    db.transaction(function(tx) {
+
+        //create table
+        tx.executeSql("SELECT * FROM task", [], function(tx, res){
+          arr.push(new Task(res.rows.item(i).day,
+            res.rows.item(i).title),
+            res.rows.item(i).date_task);
+
+        }
+      }
+      return [].contact(arr);
+  }
+
+}
